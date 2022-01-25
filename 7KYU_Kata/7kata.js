9923723edb0c4e78890a4231a94ef2a7d75975ca
Elie uwimana <muhizielie01@gmail.com>
2022-01-25 07:37:39 +0200

    add:7kyu code katas

---
+++ 7KYU_Kata/7kata.js
@@ -0,0 +1,14 @@
+/* 
+1.
+Given an input of an array of digits, return the array with each digit incremented by 1, the second digit by2, etc.
+make sure to start counting your positions from 1(and not 0).
+your result can only contain single digit number, only the last digit of the number should be returned.
+
+NOTES:
+    . return an empty array if your array is empty.
+    .Array will only contain numbers so don't worry about checking that.
+
+*/
+
+const ArrDigits = [];
+
