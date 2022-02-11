97c661ba15778b39c6c449c773fa7eada2c972b1
Elie uwimana <muhizielie01@gmail.com>
2022-02-11 19:01:34 +0200

    add: adding some changes

--- 7KYU_Kata/7kata.js
+++ 7KYU_Kata/7kata.js
@@ -9,6 +9,99 @@ NOTES:
     .Array will only contain numbers so don't worry about checking that.
 
 */
+//--------------1. display element of the array incremented by their indices----------
 
-const ArrDigits = [];
+const ArrDigits = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const arrayFunction = (arr) => {
+    let arrResult = [];
+    for(digit of arr){
+        arrResult.push((arr.indexOf(digit) + 1 ) + digit);
+    }
+    return arrResult;
+}
+const myDigits = arrayFunction(ArrDigits);
+
+console.log(myDigits)
+//-------------2. check the output of the first part---------------
+
+
+
+
+// const descendingOrder = (n) => {
+//     const ArrOrder = String(n).split("").sort((a, b) => a < b ? 1 : -1).join("");
+//     return ArrOrder;
+// }
+// console.log(descendingOrder(42145));
+
+
+const descendingOrder = (n) => {
+    const ArrOrder = parseInt(String(n).split("").sort().reverse().join(""));
+    return ArrOrder;
+}
+console.log(descendingOrder(87094371));
+
+
+
+
+
+3.
+/* 
+This time no story, no theory. The examples below show you how to write function accum:
+
+Examples:
+accum("abcd") -> "A-Bb-Ccc-Dddd" accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy" 
+accum("cwAt") -> "C-Ww-Aaa-Tttt"
+The parameter of accum is a string which includes only letters from a..z and A..Z.
+
+*/
+
+
+//1. create a function that you pass a string
+//2. loop throught the string and split it 
+//2.create another 
+
+
+// const accum = (stringAlpha) => {
+//     stringAlpha.split("").forEach(el =>{
+//         console.log(`${el.toUpperCase().repeat(el.indexOf(el + 1))} - ${el.indexOf(el)} `)
+//     });
+// }
+// console.log(accum("abds"));
+
+
+const mea = ["a", "b", "c", "g"];
+const answers = mea.forEach(el => {
+    el.repeat(mea[el] + 1)
+});
+
+console.log(answers)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
--- 8KYU_kata/8kata.js
+++ 8KYU_kata/8kata.js
@@ -39,13 +39,60 @@ const reverseSequence = (n) => {
 console.log(reverseSequence(15))
 
 //Also this can work
-const reverseSequence = (n) => {
-    const arrayOfSequence = [];
-    for(let el = 1; el <= n; el++){
-        arrayOfSequence.push(el)
+// const reverseSequence = (n) => {
+//     const arrayOfSequence = [];
+//     for(let el = 1; el <= n; el++){
+//         arrayOfSequence.push(el)
+//     }
+//     return arrayOfSequence.reverse()
+// }
+
+// console.log(reverseSequence(15))
+
+//this also
+// const reverseSequence = n => {
+//     return Array(n).fill(0).map((e, i) => n - i );
+// }
+
+// console.log(reverseSequence(5));
+
+//this as well
+// const reverseSeq = length => Array.from({length}, () => length--)
+
+
+
+
+/* 
+You are given an array (which will have a length of at least 3, but could be very large) containing integers. The array is either entirely comprised of odd integers or entirely comprised of even integers except for a single integer N. Write a method that takes the array as an argument and returns this "outlier" N.
+
+Examples
+[2, 4, 0, 100, 4, 11, 2602, 36]
+Should return: 11 (the only odd number)
+
+[160, 3, 1719, 19, 11, 13, -21]
+Should return: 160 (the only even number)
+*/
+
+const findOutlier = integers => {
+    const oddIntegers = [];
+    const evenIntegers = [];
+    for(let el of integers){
+        if(el % 2 === 0){
+            evenIntegers.push(el)
+        } else if (el % 2 !== 0){
+            oddIntegers.push(el)
+        }
     }
-    return arrayOfSequence.reverse()
-}
+    if(oddIntegers || evenIntegers === integers){
+        return integers;
+    } else if(evenIntegers > oddIntegers){
+        return oddIntegers;
+    } else if(oddIntegers > evenIntegers ){
+        return evenIntegers;
+    }
 
-console.log(reverseSequence(15))
+};
 
+const outlierNumber = findOutlier([252, 4721, 10, 100, 4, 110, 2602, 6]);
+
+console.log(`The outlier here is ${outlierNumber}`)
